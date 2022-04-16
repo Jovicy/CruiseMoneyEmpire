b14696cd63433a1d154a874aeb5a42c259be46b2
Bamidele <bamsjovicy@gmail.com>
2022-04-17 00:53:21 +0100

    made some few corrections

---
+++ js/question.js
@@ -0,0 +1,27 @@
+const quiz = [
+    {
+        q:'Do you know if you have a smartphone you are a potential millionaire ?',
+        options:['yes', 'no'],
+        answer:0
+    },
+    {
+        q:'Have you thinked of having a savings ?',
+        options:['true', 'false'],
+        answer:0
+    },
+    {
+        q:'Have you thinked of your finacial status ?',
+        options:['true', 'false'],
+        answer:0
+    },
+    {
+        q:'Have you thinked of the secret ?',
+        options:['true', 'false'],
+        answer:0
+    },
+    {
+        q:'Have you contacted me for the secret ?',
+        options:['yes', 'no'],
+        answer:0
+    }
+]
